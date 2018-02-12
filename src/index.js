3dea9c411fb0d922b05cc15afbb3e617174214c2
Emily Wise <wise.m.emily@gmail.com>
2018-02-12 11:46:25 -0700

    added post method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const getTopWord = () => {
   .catch((error) => console.error({errors}));
 }
 
+const addWord = (newWord) => {
+  var data = {word: { value: newWord }}
+  return fetch(`https://wordwatch-api.herokuapp.com/api/v1/words`,{
+  method: 'post',
+    headers:
+      { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+  .then((response) => handleResponse(response))
+  .then((x) => console.log(x))
+  .catch(error => console.log({ error }))
+};
+
 const handleResponse = (response) => {
   return response.json()
   .then(json => {
@@ -30,11 +43,31 @@ const addTopWord = (topWord) => {
 }
 
 const renderTopWord = (word, count) => {
-  $('.top-word').append(`<h3>  ${word} </h3>`)
-  $('.word-count').append(`<span>${count}</span>`)
+  $('.top-word').append(`<h3>  ${word} (${count})</h3>`)
 }
+const countText = (event) => {
+   event.preventDefault()
+  let sanitizedText = {};
+  var newTextInput = document.getElementById("textareabox").value.split(" ")
+  newTextInput.forEach(function(word){
+    if(sanitizedText[word] === undefined){
+      sanitizedText[word] = 1;
+      }
+    else
+    sanitizedText[word] += 1
 
+    })
+
+  var words = Object.keys(sanitizedText)
+  var frequency = Object.values(sanitizedText)
+  words.forEach(function(word){
+  $('.word-count').append(`<p> ${word}</p>`)
+  addWord(word);
+  })
+
+}
 
 $(document).ready(() => {
   getTopWord();
+$('.text-submission').on('click', '.break-down-btn', countText);
 })
