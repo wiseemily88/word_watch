3c5cbde5bb691a322baf39ff84fb79c0904e1afc
Emily Wise <wise.m.emily@gmail.com>
2018-02-12 10:07:23 -0700

    complete first user story

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import $ from 'jquery'
 
+const getTopWord = () => {
+  return fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
+  .then(response => handleResponse(response))
+  .then(topWord => addTopWord(topWord))
+  .catch((error) => console.error({errors}));
+}
+
+const handleResponse = (response) => {
+  return response.json()
+  .then(json => {
+    if(!response.ok) {
+      const error = {
+      status: response.status,
+      statusTest: response.statusTest,
+      };
+    return Promise.reject(error);
+    }
+    return json;
+  })
+}
+
+const addTopWord = (topWord) => {
+
+  var wordObject = topWord.word
+  var word = Object.keys(wordObject)[0]
+  var count = Object.values(wordObject)[0]
+  return renderTopWord(word, count)
+}
+
+const renderTopWord = (word, count) => {
+  $('.top-word').append(`<h3>  ${word} </h3>`)
+  $('.word-count').append(`<span>${count}</span>`)
+}
+
+
 $(document).ready(() => {
-  // have fun!
+  getTopWord();
 })
